0ade0c81ccc52080103a30ebf3339c2da4d8f32c
Abdul Rafay <rafeessiddiqui79@gmail.com>
2025-11-23 10:01:51 -0800

    Delete fig02_01.c

--- fig02_01.c
+++
@@ -1,9 +0,0 @@
-// fig02_01.c
-// A first program in C.
-#include <stdio.h>
-
-// function main begins program execution 
-int main(void) {
-   printf("Welcome to C Language!\n");
-} // end function main 
-
